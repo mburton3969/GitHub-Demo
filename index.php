afc37eb54a1f7caee27b5f10388de4d7996a45a2
BrookeAlexandraMedia <49649945+BrookeAlexandraMedia@users.noreply.github.com>
2019-04-15 11:48:37 -0400

    Update index.php
    took out "again for 3rd time"

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<div>
 		<h1>This is a demo site for testing</h1>
 		<p>My name is Brooke Tweddell.<p>
-			<p>Testing the restrictions again for the 3rd time</p>
+			<p>Testing the restrictions.</p>
 	</div>
 </body>
 </html>
