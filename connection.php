ef72cf1d4a48c118e8e166410e24dc36e4852b0b
Brooke Tweddell <brookealexandramedia.com>
2019-06-06 15:03:15 -0400

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+		//Database connection credentials...
+		$host = 'localhost';
+		$user = 'root';
+		$pass = 'root';
+		$db = 'demo';
+		//Makes connection to the database...
+		$conn = mysqli_connect($host,$user,$pass,$db) or die($conn->error);
+
+		
+		?>
