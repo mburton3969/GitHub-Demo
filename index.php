e341463826d5c237287b7e0cba3d4f6f693c32da
Brooke Tweddell <brookealexandramedia.com>
2019-06-06 12:12:03 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php 
+ $user_name = "Michael Burton.";
+ ?>
+
+
 <html>
 <head>
 	<title>Demo Site</title>
@@ -5,7 +10,7 @@
 <body>
 	<div>
 		<h1>This is a demo site for testing</h1>
-		<p>My name is Brooke Tweddell.<p>
+		<p>My name is <?php echo $user_name; ?><p>
 	</div>
 </body>
 </html>
